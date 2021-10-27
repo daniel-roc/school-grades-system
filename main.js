fb9fb6037e21d91c2315276931d3769c45340c35
Daniel Rocha <daniel.rocha@outlook.com.br>
2021-10-26 23:12:18 -0300

    function created

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
   ### Transformar notas escolares
 
-  Crie um algoritmo que transforme as notas do sistemas numérico para sistema de notas em caracteres tipo A B ChannelSplitterNode
+  Crie um algoritmo que transforme as notas do sistemas numérico para sistema de notas em caracteres tipo A B C
 
   * de 90 para cima -   A
   * entre 80 - 89 -     B
@@ -9,4 +9,23 @@
   * entre 60 - 69 -     D
   * menor que 60 -      F
  */
-console.log('Olá de novo')
+
+function gradeConversor(grade) {
+  let result
+
+  if (grade >= 90) {
+    result = 'A'
+  } else if (grade < 90 && grade >= 80) {
+    result = 'B'
+  } else if (grade < 80 && grade >= 70) {
+    result = 'C'
+  } else if (grade < 70 && grade >= 60) {
+    result = 'D'
+  } else {
+    result = 'F'
+  }
+
+  return result
+}
+
+console.log(gradeConversor(95))
